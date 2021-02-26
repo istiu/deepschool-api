7eb6ab3086601d8bd84d2d56700f4c23f6b668e4
Robert Santos <rbertt.js@gmail.com>
2021-02-25 21:13:22 -0300

    database info from .env file

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'deepschool'),
-        username: env('DATABASE_USERNAME', 'deepschool'),
-        password: env('DATABASE_PASSWORD', 'deepschool123'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
